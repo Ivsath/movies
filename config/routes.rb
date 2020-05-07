366c260c63fa7a4f93d7b080c2e761a39d554f17
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-07 15:16:07 -0500

    Add action and view to create new movies

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'movies#index'
 
-  get 'movies' => 'movies#index'
-  get 'movies/:id' => 'movies#show', as: 'movie'
-  get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
-  patch 'movies/:id' => 'movies#update'
+  # get 'movies' => 'movies#index'
+  # get 'movies/new' => 'movies#new'
+  # get 'movies/:id' => 'movies#show', as: 'movies'
+  # get 'movies/:id/edit' => 'movies#edit', as: 'edit_movie'
+  # patch 'movies/:id' => 'movies#update'
+
+  resources :movies
 end
