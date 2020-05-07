57deea0e894a97e1df49d127a2e1a1138ef1cf1e
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-07 17:31:11 -0500

    Create destroy action for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -31,6 +31,13 @@ class MoviesController < ApplicationController
     redirect_to @movie
   end
 
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+
+    redirect_to movies_url
+  end
+
   private
 
   def movie_params
