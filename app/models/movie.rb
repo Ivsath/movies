046be1b2fec19dab9e082c90005dd159af8765b6
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-07 23:14:47 -0500

    Rubocop fix

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class Movie < ApplicationRecord
-  RATINGS = %w(G PG PG-13 R NC-17)
+  RATINGS = %w[G PG PG-13 R NC-17].freeze
 
   validates :title, :released_on, :duration, presence: true
   validates :description, length: { minimum: 25 }
   validates :total_gross, numericality: { greater_than_or_equal_to: 0 }
   validates :image_file_name, format: {
-      with: /\w+\.(jpg|png)\z/i,
-      message: "Must be a JPG or PNG image."
+    with: /\w+\.(jpg|png)\z/i,
+    message: 'Must be a JPG or PNG image.'
   }
   validates :rating, inclusion: { in: RATINGS }
 
