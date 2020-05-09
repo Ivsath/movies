aa3b8ab5d5c7d53bc77434adb21111e721b0d8f3
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-09 14:44:55 -0500

    Show average stars

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -13,4 +13,12 @@ module MoviesHelper
     # movie.released_on.strftime('%Y')
     movie.released_on.year
   end
+
+  def average_stars(movie)
+    if movie.average_stars.zero?
+      content_tag(:strong, 'No reviews')
+    else
+      pluralize(number_with_precision(movie.average_stars, precision: 1), 'star')
+    end
+  end
 end
