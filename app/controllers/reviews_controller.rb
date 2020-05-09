4c5213a8563c8537f4dc651525bf8f086e34f875
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-09 11:59:16 -0500

    Remove code duplication

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
 
     if @review.save
@@ -24,5 +23,9 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:name, :comment, :stars)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
