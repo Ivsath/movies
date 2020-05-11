2c46dc9087c90dfadb2819e3bb41d4d642401afd
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-11 13:30:22 -0500

    Create sessions controller and sign-in form

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  resource :session, only: [:new, :create, :destroy]
+
   resources :users
   get "signup" => "users#new"
+  get "signin" => "sessions#new"
 end
