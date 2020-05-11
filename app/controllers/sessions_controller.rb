1e33609f0b593844c236fb92e5a94f20f51d4b18
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-11 14:51:58 -0500

    Implement user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      # flash[:notice] = "Welcome back, #{user.name}!"
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
   end
 
   def destroy
