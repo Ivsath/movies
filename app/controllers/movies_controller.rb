2d196351ad21b168aa576b4fbe6266e5218c05fc
Irvin Reyes <reyes.yerp@gmail.com>
2020-05-12 21:48:13 -0500

    Add check boxes in the movie form to add and remove genres

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -58,6 +58,6 @@ class MoviesController < ApplicationController
   def movie_params
     params.require(:movie)
           .permit(:title, :description, :rating, :released_on, :total_gross,
-                  :director, :duration, :image_file_name)
+                  :director, :duration, :image_file_name, genre_ids: [])
   end
 end
